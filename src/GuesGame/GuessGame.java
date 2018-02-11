1c643524d590a1206d93cc71041fd9e5d3f52c42
AlexanderRanks <AlexanderRanks@gmail.com>
2018-02-11 19:50:05 +0200

    угадайка - some house keeping

--- src/GuesGame/GuessGame.java
+++ src/GuesGame/GuessGame.java
@@ -6,39 +6,39 @@ import java.util.Scanner;
 // Компьютер пытается его отгадать. На каждую попытку компьютера игрок отвечает `>`, `<` или `=`
 public class GuessGame {
     public static void main(String[] args) {
-        int wishMin=1;
-        int wishMax=100;
+        int wishMin = 1;
+        int wishMax = 100;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Think of any nymber between " + wishMin + " and " + wishMax);
         System.out.println("When ready press ENTER.");
         scanner.nextLine();
 
-        int newGuess=0;
+        boolean solved = false;
         do {
 
-            if(wishMin==wishMax){
-                System.out.println("You are thinking of: "  + wishMin);
-                newGuess = 0;
-            }else {
-                newGuess = (wishMin+wishMax)/2;
-                System.out.println("Are you thinking of: "  + newGuess+"?");
+            if (wishMin == wishMax) {
+                System.out.println("You are thinking of: " + wishMin);
+                solved = true;
+            } else {
+                int newGuess = (wishMin + wishMax) / 2;
+                System.out.println("Are you thinking of: " + newGuess + "?");
                 String userInput;
                 do {
                     System.out.println("[<,>,=]");
                     userInput = scanner.nextLine();
-                } while ((!userInput.equals("="))&&(!userInput.equals("<"))&&(!userInput.equals(">")));
+                } while ((!userInput.equals("=")) && (!userInput.equals("<")) && (!userInput.equals(">")));
 
-                if (userInput.equals("<")){
-                    wishMax = newGuess-1;
-                }else if(userInput.equals(">")){
-                    wishMin = newGuess+1;
-                }else{
-                    newGuess = 0;
+                if (userInput.equals("<")) {
+                    wishMax = newGuess - 1;
+                } else if (userInput.equals(">")) {
+                    wishMin = newGuess + 1;
+                } else {
+                    solved = true;
                 }
 
             }
-        }while(newGuess!=0);
+        } while (!solved);
 
         scanner.close();
     }
